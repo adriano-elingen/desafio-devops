59606e0d5664edace7bccb149890faf4e6b1e9fa
Adriano Henrique Elingen <adriano.elingen@fesctecnologia.com.br>
2021-11-28 15:02:03 -0300

    erro variavel

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc" {
   name = "${var.project_name}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.region}a", "${var.region}b"]
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
