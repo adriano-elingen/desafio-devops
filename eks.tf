3e14ddf1d4e6f9e11d996e9c2388fbf470eec306
Adriano Henrique Elingen <adriano.elingen@fesctecnologia.com.br>
2021-11-28 14:13:33 -0300

    add eks

---
+++ eks.tf
@@ -0,0 +1,30 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_id
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+
+  cluster_version = "1.21"
+  cluster_name    = "${var.project_name}"
+  vpc_id          = "${module.vpc.vpc_id}"
+  subnets         = ["${element(module.vpc.private_subnets, 0)}", "${element(module.vpc.private_subnets, 1)}"]
+
+  worker_groups = [
+    {
+      instance_type = "t2.micro"
+      asg_min_size  = 1
+      asg_max_size  = 5
+    }
+  ]
+}
