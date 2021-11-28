083317c6f0b6ecbc3ce0180b2cbe4217290db916
Adriano Henrique Elingen <adriano.elingen@fesctecnologia.com.br>
2021-11-28 13:34:23 -0300

    teste vpc

---
+++ output.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+  description = "ID do VPC do projeto"
+  value = module.vpc.vpc_id
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "Região AWS"
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  description = "String para nomear recursos"
+  default = "devops-challenge"
+}
