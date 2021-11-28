da1f56e600f8a70cfac74591ee3ad7358cd99fed
Adriano Henrique Elingen <adriano.elingen@fesctecnologia.com.br>
2021-11-28 17:15:05 -0300

    kubeconfig output

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "eks_endpoint" {
   description = "Endpoint do cluster EKS"
   value = module.eks.cluster_endpoint
 }
+
+output "eks_kubeconfig" {
+  description = "Kubeconfig do cluster EKS"
+  value = module.eks.kubeconfig
+}
