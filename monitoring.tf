4687186611b9f6115d44fb64e6713ae57834b070
Adriano Henrique Elingen <adriano.elingen@fesctecnologia.com.br>
2021-11-28 21:07:36 -0300

    sets grafana

--- monitoring.tf
+++ monitoring.tf
@@ -13,6 +13,26 @@ resource "helm_release" "loki_stack" {
   repository       = "https://grafana.github.io/helm-charts"
   chart            = "loki-stack"
 
+  set {
+    name  = "grafana.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "prometheus.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "fluent-bit.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "grafana.service.type"
+    value = "LoadBalancer"
+  }
+
   depends_on = [module.eks]
 
 }
