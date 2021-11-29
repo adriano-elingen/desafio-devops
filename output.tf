63b9cdfb6f8a2080d7a74307680721ff60ae60e7
Adriano Henrique Elingen <adriano.elingen@fesctecnologia.com.br>
2021-11-28 22:03:12 -0300

    corrigido output

--- output.tf
+++ output.tf
@@ -12,8 +12,3 @@ output "nginx_lb_info" {
   description = "informacoes LB pod teste"
   value = kubernetes_service.simple-nginx-service.status.0.load_balancer.0.ingress.0.hostname
 }
-
-output "grafana_lb_info" {
-  description = "informacoes LB grafana"
-  value = kubernetes_service.loki-grafana.status.0.load_balancer.0.ingress.0.hostname
-}
