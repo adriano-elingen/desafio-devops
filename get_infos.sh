f92161df2e4197dcd7541cc7724667035f03824e
Adriano Henrique Elingen <adriano.elingen@fesctecnologia.com.br>
2021-11-28 22:43:10 -0300

    script auxiliar

---
+++ get_infos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+GRAFANA_USER="admin"
+GRAFANA_SENHA="$(kubectl get secret --namespace monitoring loki-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo)"
+GRAFANA_URL="$(kubectl get service --namespace monitoring loki-grafana | awk {'print $5'})"
+
+NGINX_URL="$(kubectl get service --namespace monitoring loki-grafana | awk {'print $5'})"
+
+echo -e "URL pod teste: \n"
+echo "http://$NGINX_URL"
+
+
+echo -e "\nAcesso Grafana: \n"
+echo "http://$GRAFANA_URL"
+echo "User: $GRAFANA_USER"
+echo "Password: $GRAFANA_SENHA"
--- output.tf
+++ output.tf
@@ -1,8 +1,3 @@
-output "eks_endpoint" {
-  description = "Endpoint do cluster EKS"
-  value = module.eks.cluster_endpoint
-}
-
 output "eks_kubeconfig" {
   description = "Kubeconfig do cluster EKS"
   value = module.eks.kubeconfig
