0d22f8f54fc277af06b66e1063d5f9273a80c9ed
Adriano Henrique Elingen <adriano.elingen@fesctecnologia.com.br>
2021-11-28 22:04:50 -0300

    undo outputs

--- output.tf
+++ output.tf
@@ -7,8 +7,3 @@ output "eks_kubeconfig" {
   description = "Kubeconfig do cluster EKS"
   value = module.eks.kubeconfig
 }
-
-output "nginx_lb_info" {
-  description = "informacoes LB pod teste"
-  value = kubernetes_service.simple-nginx-service.status.0.load_balancer.0.ingress.0.hostname
-}
